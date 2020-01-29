f80aed110e847ddeb1106be75e03185e6b729e15
Kobi Jones <kobijonesmail@gmail.com>
2020-01-29 17:11:49 -0600

    Added separate member variables for each network table entry for more reliable access

--- src/main/java/frc/robot/subsystems/Queue.java
+++ src/main/java/frc/robot/subsystems/Queue.java
@@ -11,13 +11,16 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Queue extends SubsystemBase {
-  private final NetworkTable m_networkTable;
+  public final NetworkTable m_networkTable;
+  public final NetworkTableEntry m_isClosedEntry;
+  private final NetworkTableEntry m_runSpeedEntry;
 
   private final int m_runMotorID = -1;
   private final TalonSRX m_runMotor;
@@ -33,7 +36,9 @@ public class Queue extends SubsystemBase {
    * Creates a new Queue.
    */
   public Queue() {
-    m_networkTable = NetworkTableInstance.getDefault().getTable(getTableName());
+    m_networkTable = NetworkTableInstance.getDefault().getTable(getName());
+    m_runSpeedEntry = m_networkTable.getEntry("Run speed");
+    m_isClosedEntry = m_networkTable.getEntry("Is closed");
 
     m_runMotor = new TalonSRX(m_runMotorID);
     m_runMotor.configFactoryDefault();
@@ -43,13 +48,8 @@ public class Queue extends SubsystemBase {
 
   @Override
   public void periodic() {
-    m_networkTable.getEntry("Run speed").setNumber(m_runMotor.getMotorOutputPercent());
-    m_networkTable.getEntry("Is closed").setBoolean(getIsClosed());
-
-  }
-
-  public static String getTableName() {
-    return Queue.class.getSimpleName();
+    m_runSpeedEntry.setNumber(m_runMotor.getMotorOutputPercent());
+    m_isClosedEntry.setBoolean(getIsClosed());
   }
 
   public void run(double speed) {
