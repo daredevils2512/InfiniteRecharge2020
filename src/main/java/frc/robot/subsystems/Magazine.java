084de89af8f24e507f12c410a77d85195cd01c9d
Toby <tmkoth431@isd709.org>
2020-02-19 13:48:27 -0600

    made more loggers

--- src/main/java/frc/robot/subsystems/Magazine.java
+++ src/main/java/frc/robot/subsystems/Magazine.java
@@ -127,9 +127,9 @@ public class Magazine extends PropertySubsystem {
 
     int newCount = m_powerCellCount + deltaCount;
     if (newCount < 0)
-      logger.log(Level.WARNING, "Power cell count exceeded lower bounds");
+      logger.warning("Power cell count exceeded lower bounds");
     else if (newCount > 3)
-      logger.log(Level.WARNING, "Power cell count exceeded upper bounds");
+      logger.warning("Power cell count exceeded upper bounds");
 
     m_powerCellCount = MathUtil.clamp(newCount, 0, 3);
     if (deltaCount != 0)
