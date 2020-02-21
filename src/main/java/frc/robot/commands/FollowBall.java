6fdf7a3780e15726ecb0598c3a2789a6ed0aab7e
Kobi Jones <kobijonesmail@gmail.com>
2020-02-21 16:16:37 -0600

    Removed conficting (old) limelight code

--- src/main/java/frc/robot/commands/FollowBall.java
+++ src/main/java/frc/robot/commands/FollowBall.java
@@ -7,59 +7,40 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.vision.Limelight;
-import frc.robot.vision.Limelight.Pipeline;
+import frc.robot.vision.LimelightPipeline;
 
 public class FollowBall extends CommandBase {
   private Drivetrain m_drivetrain;
-  private Limelight m_limelight;
-  private Pipeline m_pipeline;
+  private LimelightPipeline m_pipeline;
 
   //constants to change the moving
-  private final double k_move = 0.2;
-  private final double k_turn = 0.03;
-  private final double targetArea = 70;
+  private final double m_maxMove = 0.2;
+  private final double m_maxTurn = 0.03;
+  private final double m_targetFill = 0.7;
 
-  //move and turn
-  private double move;
-  private double turn;
-
-  public FollowBall(Drivetrain drivetrain, Limelight limelight, Pipeline pipeline) {
-    m_limelight = limelight;
-    m_pipeline = pipeline;
+  public FollowBall(Drivetrain drivetrain, LimelightPipeline pipeline) {
     m_drivetrain = drivetrain;
+    m_pipeline = pipeline;
     addRequirements(m_drivetrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_limelight.setPipeline(m_pipeline);
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (m_limelight.hasTarget()) {
-      move = -Math.min(((targetArea - m_limelight.ta()) * k_move), 0.8);
-      turn = -(m_limelight.tx() * k_turn) <= 0.7 && m_limelight.tx() * k_turn >= -0.7 ? 
-        m_limelight.tx() * k_turn : 0.5 * Math.signum(m_limelight.tx());
-    } else {
-      turn = -0.5 * Math.signum(m_limelight.getLastPosition());
-      move = 0.1;
-    }
-    SmartDashboard.putNumber("move", move);
-    SmartDashboard.putNumber("turn", turn);
-    m_drivetrain.simpleArcadeDrive(move, turn);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_drivetrain.simpleArcadeDrive(0.0, 0.0);
+    m_drivetrain.simpleArcadeDrive(0, 0);
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/vision/LimelightUtil.java
+++ src/main/java/frc/robot/vision/LimelightUtil.java
@@ -1,7 +1,38 @@
 package frc.robot.vision;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 public class LimelightUtil {
   private LimelightUtil() {
     
+  }
+
+  public static boolean hasTarget(LimelightPipeline pipeline) {
+    usePipeline(pipeline);
+    return getNetworkTable(pipeline).getEntry("tv").getNumber(0).intValue() == 1;
+  }
+
+  public static double getHorizontalOffset(LimelightPipeline pipeline) {
+    usePipeline(pipeline);
+    return getNetworkTable(pipeline).getEntry("tx").getNumber(0).doubleValue();
+  }
+
+  public static double getVerticalOffset(LimelightPipeline pipeline) {
+    usePipeline(pipeline);
+    return getNetworkTable(pipeline).getEntry("ty").getNumber(0).doubleValue();
+  }
+
+  public static double getFill(LimelightPipeline pipeline) {
+    usePipeline(pipeline);
+    return getNetworkTable(pipeline).getEntry("ta").getNumber(0).doubleValue() / 100;
+  }
+
+  private static void usePipeline(LimelightPipeline pipeline) {
+    getNetworkTable(pipeline).getEntry("pipeline").setNumber(pipeline.getID());
+  }
+
+  private static NetworkTable getNetworkTable(LimelightPipeline pipeline) {
+    return NetworkTableInstance.getDefault().getTable(pipeline.getNetworkTableName());
   }
 }
