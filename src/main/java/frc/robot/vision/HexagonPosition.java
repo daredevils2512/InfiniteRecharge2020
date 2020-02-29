4bad8de50a1623023e5e86a3543aaedc3eefcb12
unknown <ihschw241@isd709.org>
2020-02-29 17:34:23 -0600

    Revert "added new features bugs and more"
    This reverts commit b1c1df15d3d4e694eb644c02163fdabd4606ce33.

--- src/main/java/frc/robot/vision/HexagonPosition.java
+++ src/main/java/frc/robot/vision/HexagonPosition.java
@@ -7,7 +7,6 @@
 
 package frc.robot.vision;
 
-
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.util.Units;
@@ -17,7 +16,7 @@ import frc.robot.utils.DareMathUtil;
 import frc.robot.vision.Limelight;
 
 /**
- * <h1> HHHHHHHHHHHHHHH
+ * Add your docs here.
  */
 public class HexagonPosition {
     private final IDrivetrain m_drivetrain;
@@ -25,27 +24,6 @@ public class HexagonPosition {
     private final Limelight m_limelight;
     private final NetworkTable m_networkTable;
     private final double m_tolerance = 5.0; //in degrees probaly shouldnt be here but idk whatever
-
-    //constants for ball distance calculation in the form of rpm = ax^s + bx + c where x is distance in meters
-    //https://www.desmos.com/calculator/hlfz61fwpw
-    private final double a = 180.74488324;
-    private final double b = -1491.97857776;
-    private final double c = 8764.60835839;
-
-  /**
-   * notes for calculations
-   * 
-   * front of trench: status: good 
-   * distance: 4.8
-   * set rpm: 6k
-   * hood pos: 1900
-   * 
-   * 6.2 : 6500
-   * 5.7 : 6000
-   * 2.9 : 6000
-   * 4.0 : 5500
-   * 4.3 : 5700
-   */
 
     private double m_turretPosition;
     private double m_robotPosition;
@@ -64,9 +42,7 @@ public class HexagonPosition {
     }
 
     public void updatePosition() {
-        calculateShooterSpeed();
         calculatePosition();
-        m_networkTable.getEntry("required shooter speed").setDouble(calculateShooterSpeed());
         m_networkTable.getEntry("robot relative position").setDouble(getRobotRelativePosition());
         m_networkTable.getEntry("turret relative position").setDouble(getTurretRelativePosition());
     }
@@ -83,16 +59,6 @@ public class HexagonPosition {
     
     private double getTurretRelativePosition() {
         return m_robotPosition - m_drivetrain.getHeading();
-    }
-
-    /**
-     * calculates desired shooter speed using math above
-     * {@link HexagonPosition in these}
-     * @return
-     */
-    private double calculateShooterSpeed() {
-        double x = m_limelight.getDistanceToTarget();
-        return a * Math.pow(x, 2) + b * x + c;
     }
 
 }
