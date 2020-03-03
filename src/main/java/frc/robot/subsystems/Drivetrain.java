ac425c082af474fb62e2a1b65182aa6b7a6d1302
Toby <tmkoth431@isd709.org>
2020-03-02 18:28:14 -0600

    added limiting not finihed

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -231,6 +231,11 @@ public class Drivetrain extends PropertySubsystem implements IDrivetrain {
         ? new DoubleSolenoid(drivetrainMap.shiftForwardChannel, drivetrainMap.shiftReverseChannel)
         : null;
 
+      m_leftDriveMaster.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 30, 10, 0.5));
+      m_leftDriveFollower.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 30, 10, 0.5));
+      m_rightDriveMaster.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 30, 10, 0.5));
+      m_rightDriveFollower.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 30, 10, 0.5));
+
     m_kinematics = new DifferentialDriveKinematics(m_trackWidth);
     m_driveMotorFeedforward = new SimpleMotorFeedforward(m_staticGain, m_velocityGain, m_accelerationGain);
     m_odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(getYaw()));
